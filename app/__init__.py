299cf7d8db43a9a4969cbb539cf967a0820c0402
Abhishek <cse190001001@iiti.ac.in>
2020-11-16 00:35:59 +0530

    Added environment variables for sending emails

--- app/__init__.py
+++ app/__init__.py
@@ -21,10 +21,11 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = ''
-    app.config['MAIL_PASSWORD'] = ''
+    app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+    app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 
-    app.config['ADMINS'] = ['']
+    app.config['ADMINS'] = os.environ.get('MAIL_USERNAME')
     db.init_app(app=app)
     migrate.init_app(app, db)
     mail.init_app(app)
